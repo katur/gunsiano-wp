e62c1f456064c52189ebdcd3f5b82c936346aa05
Katherine Erickson <katherine.erickson@gmail.com>
2012-11-02 11:30:07 -0400

    simplified code for publications filter. added Computer Modern font files. changed main font to Comp Modern Serif, and increased font sizes. deleted accidental files in js folder.

--- wp-content/themes/gunsiano/header.php
+++ wp-content/themes/gunsiano/header.php
@@ -48,6 +48,7 @@
 <link rel="profile" href="http://gmpg.org/xfn/11" />
 <link rel="stylesheet" type="text/css" media="all" href="<?php bloginfo( 'stylesheet_url' ); ?>" />
 <link rel="pingback" href="<?php bloginfo( 'pingback_url' ); ?>" />
+<link href='http://fonts.googleapis.com/css?family=Marcellus+SC|Fenix|Arbutus+Slab|Lora|Quicksand|Marcellus|Playfair+Display+SC' rel='stylesheet' type='text/css'>
 <!--[if lt IE 9]>
 <script src="<?php echo get_template_directory_uri(); ?>/js/html5.js" type="text/javascript"></script>
 <![endif]-->
