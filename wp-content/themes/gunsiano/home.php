bbe8635b97efe919210e2581ea257167ecd1bded
Katherine Erickson <katherine.erickson@gmail.com>
2013-01-20 15:10:50 -0500

    changed home page from gallery to static

--- wp-content/themes/gunsiano/home.php
+++ wp-content/themes/gunsiano/home.php
@@ -15,65 +15,69 @@ get_header(); ?>
 			    
 			    <div id="intro">The labs of Drs. <a href="lab-members/kris-gunsalus/" title="Kris Gunsalus">Kris Gunsalus</a> and <a href="lolab-members/fabio-piano/" title="Fabio Piano">Fabio Piano</a> study the genetic and evolutionary mechanisms underlying early embryonic development.</div>
                 
-                <h1 id="research-areas" class="entry-title no-clear">Research Areas</h1>
-                <h3 id="research-area">HELLO</h3>
+                <h1 class="entry-title">Research Areas</h1>
                 
-                <div id="my-gallery">
-                    
-                    <div id="thumbnails">
-                        
-                        
-                        <!-- NETWORKS -->
-                        <a href="#" data-research-area="Biological Network Analysis" data-visual="<img src='http://localhost:8888/wp-content/uploads/2012/11/Celegans-WI8-1.jpg'>">
-                            <img src="http://localhost:8888/wp-content/uploads/2012/11/Celegans-WI8-1-150x150.jpg" alt="" title="GI" width="100" height="100" class="alignleft size-thumbnail wp-image-528" />
-                        </a>
-                        
-                        
-                        <!-- CELL BIOLOGY -->
-                        <a href="#" data-research-area="Dynamics of Cell Biology in the Early Embryo" data-visual="<video autoplay><source src='http://localhost:8888/wp-content/themes/gunsiano/video/nathalie.mp4' type='video/mp4'>Your browser does not support HTML5 video</video>">
-                            <div id="nathalie-video-wrapper">
-                                <video loop autoplay>
-                                    <source src="/wp-content/themes/gunsiano/video/nathalie.mp4" type="video/mp4">Your browser does not support HTML5 video.
-                                </video>
-                            </div>
-                        </a>
-                        
-                        
-                        <!-- EVOLUTION -->
-                        <a href="#" data-research-area="Evolution of early development and reproduction modes" data-visual="<img src='http://localhost:8888/wp-content/uploads/2012/11/mouse.jpg'>">
-                            <img src="http://localhost:8888/wp-content/uploads/2012/11/mouse-150x150.jpg" alt="" title="mouse" width="100" height="100" class="alignleft size-thumbnail" />
-                        </a>
-                        
-                        
-                        <!-- GI -->
-                        <a href="#" data-research-area="Genome-Wide Genetic Interactions Screens" data-visual="<img src='http://localhost:8888/wp-content/uploads/2012/11/Tile000001.jpg'>">
-                            <img src="http://localhost:8888/wp-content/uploads/2012/11/Tile000001-150x150.jpg" alt="" title="GI" width="100" height="100" class="alignleft size-thumbnail wp-image-528" />
-                        </a>
-                        
-                        
-                        <!-- RNA -->
-                        <a href="#" data-research-area="RNA Regulation" data-visual="<img src='http://localhost:8888/wp-content/uploads/2012/11/sw_flipped.jpg'>">
-                            <img src="http://localhost:8888/wp-content/uploads/2012/11/sw_flipped-150x150.jpg" alt="" title="GI" width="100" height="100" class="alignleft size-thumbnail" />
-                        </a>
-                        
-                        
-                        <!-- MOUSE EMBRYOGENESIS -->
-                        <a href="#" data-research-area="Mouse Embryogenesis" data-visual="<img src='http://localhost:8888/wp-content/uploads/2012/11/mouse.jpg'>">
-                            <img src="http://localhost:8888/wp-content/uploads/2012/11/mouse-150x150.jpg" alt="" title="mouse" width="100" height="100" class="alignleft size-thumbnail" />
-                        </a>                    
-                        
+                <!-- NETWORKS -->
+                <div class="research-area">
+                    <div class="research-area-text left">
+                        <h3>Biological Network Analysis</h3>
                     </div>
+                    <div class="research-area-image right">
+                        <img src='http://localhost:8888/wp-content/uploads/2012/11/Celegans-WI8-1.jpg'>
+                    </div>
+                </div>
+                
+                <!-- CELL BIOLOGY -->
+                <div class="research-area">
+                    <div class="research-area-image left">
+                        <video loop autoplay><source src='http://localhost:8888/wp-content/themes/gunsiano/video/nathalie.mp4' type='video/mp4'>Your browser does not support HTML5 video</video>
+                    </div>
+                    <div class="research-area-text right">
+                        <h3>Dynamics of Cell Biology in the Early Embryo</h3>
+                    </div>
+                </div>
+                
+                <!-- EVOLUTION -->
+                <div class="research-area">
+                    <div class="research-area-text left">
+                        <h3>Evolution of early development and reproduction modes</h3>
+                    </div>
+                        <div class="research-area-image right">
+                            <img src='http://localhost:8888/wp-content/uploads/2012/11/mouse.jpg'>
+                        </div>
+                </div>
+                
+                <!-- GI -->
+                <div class="research-area">
+                    <div class="research-area-image left">
+                        <img src='http://localhost:8888/wp-content/uploads/2012/11/Tile000001.jpg'>
+                    </div>
+                    <div class="research-area-text right">
+                        <h3>Genome-Wide Genetic Interactions Screens</h3>
+                    </div>
+                </div>
+                
+                <!-- RNA -->
+                <div class="research-area">
+                    <div class="research-area-text left">
+                        <h3>RNA Regulation</h3>
+                    </div>
+                        <div class="research-area-image right">
+                            <img src='http://localhost:8888/wp-content/uploads/2012/11/sw_flipped.jpg'>
+                        </div>
+                </div>
+                
+                <!-- MOUSE EMBRYOGENESIS -->
+                <div class="research-area">
+                    <div class="research-area-image left">
+                        <img src='http://localhost:8888/wp-content/uploads/2012/11/mouse.jpg'>
+                    </div>
+                    <div class="research-area-text right">
+                        <h3>Mouse Embryogenesis</h3>
+                    </div>
+                </div>
+                
 
-                    <div id="theater">
-                    </div>
-                </div>
-                
-                <div id="employment">
-                <h1 class="entry-title">Employment Opportunities</h1>
-                The Center for Genomics and Systems Biology at NYU Abu Dhabi is currently inviting applications for multiple Bioinformatics Software Engineer positions. These personnel will form the nucleus of a new bioinformatics core group to support multiple large-scale projects in the new NYU Abu Dhabi Center for Genomics and Systems Biology as part of a larger NYUAD Research Institute initiative. These position are NOT located in the United States. You must be willing to relocate to Abu Dhabi, United Arab Emirates. NYU Abu Dhabi is an Equal Opportunity/Affirmative Action Employer.
-                <p><p><a href="http://www.nyuopsearch.com/applicants/Central?quickFind=51346">Software Engineer position</a>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
-                    <a href="http://www.nyuopsearch.com/applicants/Central?quickFind=51345">Reseach Scientist position</a>
-                </div>
 			</div><!-- #content -->
 		</div><!-- #primary -->
 
