bc2c0e753719eec9b1341f4587e20ed8fdb48842
Katherine Erickson <katherine.erickson@gmail.com>
2013-02-05 18:44:17 -0500

    added images for Protorhabditis and contact page

--- wp-content/themes/gunsiano/js/publications.js
+++ wp-content/themes/gunsiano/js/publications.js
@@ -5,7 +5,7 @@ $(function(){
 
 selectDefaultPublications = function(){
 	$("[data-author]").trigger("click");
-	$("[data-date]").slice(0,2).trigger("click");
+	$("[data-date]").slice(0,1).trigger("click");
 }
 
 filterPublications = function(){
@@ -27,3 +27,34 @@ filterPublications = function(){
 		});
 	});
 }
+
+/*
+toggleDate = function(){
+	$("[data-date]").click(function(e){
+		e.preventDefault(); // keeps page stationary after click
+		$("[data-data]").removeClass("active"); // unset previous active link
+		$(this).addClass("active"); // set this link to active
+		updateAddress();
+	});
+}
+
+filterPublications = function(){
+	$("[data-author]").click(function(e){
+		e.preventDefault(); // keep page stationary after click
+		$(this).toggleClass("active"); // activate or unactivate the link
+		$(".publication").hide(); // hide all publications
+		$(".active[data-author]").each(function(){
+			author = $(this).attr("data-author");
+			$(".active[data-date]").each(function(){
+				date = $(this).attr("data-date");
+				$(".publication").each(function(){
+					if ($(this).find(".authors").text().match(author) &&
+					$(this).find(".date").text().match(date)) {
+						$(this).show();
+					}
+				});
+			});
+		});
+	});
+}
+*/
